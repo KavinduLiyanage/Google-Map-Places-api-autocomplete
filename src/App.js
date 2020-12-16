779d5f52734b23c7f5847c60e3bd4b0f8b58f321
KavinduLiyanage <kavindu@orelit.com>
2020-12-16 13:55:48 +0530

    Implemented Simple Search using Google Places Api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PlacesAutocomplete from "react-places-autocomplete";
+import scriptLoader from "react-async-script-loader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function App({ isScriptLoaded, isScriptLoadSucceed }) {
+  const [address, setAddress] = React.useState("");
+
+  const handleChange = (value) => {
+    setAddress(value)
+  }
+
+  const handleSelect = (value) => {
+    setAddress(value)
+  }
+
+  if (isScriptLoaded && isScriptLoadSucceed) {
+    return (
+      <div>
+        <PlacesAutocomplete
+          value={address}
+          onChange={handleChange}
+          onSelect={handleSelect}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading,
+          }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: "Enter Address...",
+                })}
+              />
+              <div>
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion) => {
+                  const style = suggestion.active
+                    ? { backgroundColor: "#a83232", cursor: "pointer" }
+                    : { backgroundColor: "#ffffff", cursor: "pointer" };
+
+                  return (
+                    <div {...getSuggestionItemProps(suggestion, { style })}>
+                      {suggestion.description}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
-export default App;
+export default scriptLoader([
+  `https://maps.googleapis.com/maps/api/js?key=AIzaSyBDQx_BEYY_kl3sj_YjfovX0hk3qO9hWOY&libraries=places`,
+])(App);
